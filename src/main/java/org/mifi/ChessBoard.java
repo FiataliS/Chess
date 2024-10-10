58778713268ce651a7b1f195c4e3db971d7a174e
FiataliS <FiataliS@gmail.com>
2024-10-10 15:05:46 +0500

    add castling0 and 7. and add code

--- src/main/java/org/mifi/ChessBoard.java
+++ src/main/java/org/mifi/ChessBoard.java
@@ -21,6 +21,10 @@ public class ChessBoard {
                 board[endLine][endColumn] = board[startLine][startColumn]; // if piece can move, we moved a piece
                 board[startLine][startColumn] = null; // set null to previous cell
                 this.nowPlayer = this.nowPlayerColor().equals("White") ? "Black" : "White";
+                if (board[endLine][endColumn].getSymbol().equals("R") || board[endLine][endColumn].getSymbol().equals("K")) {
+                    board[endLine][endColumn].check = false;
+                }
+
                 return true;
             } else return false;
         } else return false;
@@ -52,11 +56,68 @@ public class ChessBoard {
         return pos >= 0 && pos <= 7;
     }
 
-    public boolean castling7() {
-        return true;
+    public boolean castling0() {
+        if (nowPlayer.equals("White")) {
+            if (board[0][0] == null || board[0][4] == null) return false;
+            if (board[0][0].getSymbol().equals("R") && board[0][4].getSymbol().equals("K") && board[0][1] == null && board[0][2] == null && board[0][3] == null) {
+                if (board[0][0].getColor().equals("White") && board[0][4].getColor().equals("White") && board[0][0].check && board[0][4].check && new King("White").isUnderAttack(this, 0, 2)) {
+                    board[0][4] = null;
+                    board[0][2] = new King("White");
+                    board[0][2].check = false;
+                    board[0][0] = null;
+                    board[0][3] = new Rook("White");
+                    board[0][3].check = false;
+                    nowPlayer = "Black";
+                    return true;
+                } else return false;
+            } else return false;
+        } else {
+            if (board[7][0] == null || board[7][4] == null) return false;
+            if (board[7][0].getSymbol().equals("R") && board[7][4].getSymbol().equals("K") && board[7][1] == null && board[7][2] == null && board[7][3] == null) {
+                if (board[7][0].getColor().equals("Black") && board[7][4].getColor().equals("Black") && board[7][0].check && board[7][4].check && new King("Black").isUnderAttack(this, 7, 2)) {
+                    board[7][4] = null;
+                    board[7][2] = new King("Black");
+                    board[7][2].check = false;
+                    board[7][0] = null;
+                    board[7][3] = new Rook("Black");
+                    board[7][3].check = false;
+                    nowPlayer = "White";
+                    return true;
+                } else return false;
+            } else return false;
+        }
     }
 
-    public boolean castling0() {
-        return true;
+    public boolean castling7() {
+        if (nowPlayer.equals("Black")) {
+            if (board[0][0] == null || board[0][4] == null) return false;
+            if (board[0][0].getSymbol().equals("R") && board[0][4].getSymbol().equals("K") && board[0][1] == null && board[0][2] == null && board[0][3] == null) {
+                if (board[0][0].getColor().equals("Black") && board[0][4].getColor().equals("Black") && board[0][0].check && board[0][4].check && new King("Black").isUnderAttack(this, 0, 2)) {
+                    board[0][4] = null;
+                    board[0][2] = new King("Black");
+                    board[0][2].check = false;
+                    board[0][0] = null;
+                    board[0][3] = new Rook("Black");
+                    board[0][3].check = false;
+                    nowPlayer = "White";
+                    return true;
+                } else return false;
+            } else return false;
+        } else {
+            if (board[7][0] == null || board[7][4] == null) return false;
+            if (board[7][0].getSymbol().equals("R") && board[7][4].getSymbol().equals("K") && board[7][1] == null && board[7][2] == null && board[7][3] == null) {
+                if (board[7][0].getColor().equals("White") && board[7][4].getColor().equals("White") && board[7][0].check && board[7][4].check && new King("White").isUnderAttack(this, 7, 2)) {
+                    board[7][4] = null;
+                    board[7][2] = new King("White");
+                    board[7][2].check = false;
+                    board[7][0] = null;
+                    board[7][3] = new Rook("White");
+                    board[7][3].check = false;
+                    nowPlayer = "Black";
+                    return true;
+                } else return false;
+            } else return false;
+        }
     }
+
 }
